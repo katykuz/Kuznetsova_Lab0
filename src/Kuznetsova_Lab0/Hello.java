dc3040bf791149e1993d59cea351c766bf3bc786
katykuz <evk.engineer@gmail.com>
2022-07-24 10:55:17 -0700

    First commit with IntelliJ of Lab0 for CPSC5002 course at Seattle U. A HelloWorld program!

---
+++ src/Kuznetsova_Lab0/Hello.java
@@ -0,0 +1,24 @@
+/*
+ * Ekaterina Kuznetsova
+ * CPSC 5002, Seattle University
+ * This is the first submission of a package for Lab0
+ */
+package Kuznetsova_Lab0;
+
+/**
+ * This class demonstrates proper set up of IntelliJ for CPSC5002 course
+ *
+ * @author Ekaterina Kuznetsova
+ * @version 1.0
+ */
+public class Hello {
+    /**
+     * This method prints a statement to the standard output.
+     *
+     * @param args A string array containing the command line arguments.
+     */
+    public static void main(String[] args) {
+        //print greeting statement
+        System.out.println("Hello World!");
+    }
+}
